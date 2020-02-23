c6fcac86dcba1704bdb455379168c7d686611565
Douglas Galdino <dougg0k@gmail.com>
2020-02-23 16:01:13 -0300

    Updated config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,5 @@
 exports.onCreatePage = async ({ page, actions }) => {
 	const { createPage } = actions;
-	if (page.path.match(/^\/filmes/)) {
-		page.matchPath = "/filmes/*";
-		createPage(page);
-	}
 	if (page.path.match(/^\/series/)) {
 		page.matchPath = "/series/*";
 		createPage(page);
